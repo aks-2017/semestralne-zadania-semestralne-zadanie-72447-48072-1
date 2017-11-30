2c8e3d47b5d6cf1fc1e8a83490181813dc75f789
lks294 <lukass294@gmail.com>
2017-11-30 17:33:31 +0100

    setup_script
    Added script for initial setup (floodlight)

---
+++ setup_script/floodlight_setup.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+sudo apt-get update -y
+sudo apt-get install software-properties-common -y
+sudo apt-get update -y
+sudo apt-get install build-essential openjdk-7-jdk ant maven python-dev eclipse -y
+
+git clone git://github.com/floodlight/floodlight.git -b v1.1
+cd floodlight
+git submodule init
+git submodule update
+ant
+ 
+sudo mkdir /var/lib/floodlight
+sudo chmod 777 /var/lib/floodlight
+
+cd floodlight
+java -jar target/floodlight.jar
